aebeee05a37eadf04a0a345e17ea9eba24d22953
Javiergonb <javiergonb3@gmail.com>
2025-03-07 16:58:39 -0300

    Users can now be added to DB

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const express = require('express')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 const app = express()
+require('express-async-errors')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+const middleware = require('./utils/middleware')
+
+mongoose.set('strictQuery', false)
 
 mongoose.connect(config.MONGODB_URI)
   .then(() => {
@@ -17,7 +22,9 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
+app.use(middleware.errorHandler)
 module.exports = app
 
 
